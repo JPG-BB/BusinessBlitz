a6fadd823cb9fecbbe49325f8e3d709d395c3c20
leem39 <leem39@mcmaster.ca>
2016-05-05 22:56:38 -0400

    HelloWorld Test

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Paul on 2016-05-05.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("GitHub should git out");
+    }
+}
