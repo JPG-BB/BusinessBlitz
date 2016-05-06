1671970ad01cbece5e6fc50311dd18a59680d13b
leem39 <leem39@mcmaster.ca>
2016-05-05 23:48:58 -0400

    New

---
+++ Hello Pain/src/HelloPain.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Paul on 2016-05-05.
+ */
+public class HelloPain {
+    public static void main(String[] args) {
+        System.out.println("Hello pain my old friend");
+    }
+}
